cd9f021c354f8babe06e5d4c78ce70c6fdad1272
fish199902 <fish199902@8ee80806-ffd7-47eb-9445-51fd091d4666>
2009-10-14 15:09:49 +0000

    Start using SVN

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//|||||||||||||||||||||||||||||||||||||||||||||||
+
+#include "DemoApp.hpp"
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
+
+#if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
+
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+int main(int argc, char **argv)
+#endif
+{
+	try
+      {
+		DemoApp demo;
+		demo.startDemo();
+      }
+	catch(std::exception& e)
+      {
+#if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+        MessageBoxA(NULL, e.what(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+        fprintf(stderr, "An exception has occurred: %s\n", e.what());
+#endif
+      }
+
+    return 0;
+}
+
+//|||||||||||||||||||||||||||||||||||||||||||||||
